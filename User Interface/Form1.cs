3f1b932737a3480f7c2cabd715853bef408df51a
vesk4000 <veskmail@gmail.com>
2022-04-05 21:07:23 +0300

    Added project and updated gitignore file

---
+++ User Interface/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Couriers_GUI
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void tableLayoutPanel1_Click(object sender, EventArgs e)
+		{
+
+			Label label = new Label();
+			label.Text = "Hello!";
+			tableLayoutPanel1.Controls.Add(label, 0, 0);
+		}
+	}
+}
