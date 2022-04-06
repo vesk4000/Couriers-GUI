0826449787ad7b7e9bb253b252d014ca05c0aa71
vesk4000 <veskmail@gmail.com>
2022-04-06 11:01:02 +0300

    Started prototyping UI

--- User Interface/MultipleTableContainer.Designer.cs
+++ User Interface/MultipleTableContainer.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace Couriers_GUI.User_Interface
+namespace Couriers_GUI
 {
 	partial class MultipleTableContainer
 	{
@@ -30,8 +30,9 @@ namespace Couriers_GUI.User_Interface
 		private void InitializeComponent()
 		{
 			this.panel1 = new System.Windows.Forms.Panel();
-			this.dataGridView1 = new System.Windows.Forms.DataGridView();
 			this.button1 = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.label1 = new System.Windows.Forms.Label();
 			this.panel1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			this.SuspendLayout();
@@ -39,45 +40,60 @@ namespace Couriers_GUI.User_Interface
 			// panel1
 			// 
 			this.panel1.AutoScroll = true;
+			this.panel1.Controls.Add(this.label1);
 			this.panel1.Controls.Add(this.button1);
 			this.panel1.Controls.Add(this.dataGridView1);
 			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(653, 456);
+			this.panel1.Size = new System.Drawing.Size(571, 342);
 			this.panel1.TabIndex = 0;
-			// 
-			// dataGridView1
-			// 
-			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView1.Location = new System.Drawing.Point(66, 59);
-			this.dataGridView1.Name = "dataGridView1";
-			this.dataGridView1.RowHeadersWidth = 51;
-			this.dataGridView1.RowTemplate.Height = 29;
-			this.dataGridView1.Size = new System.Drawing.Size(300, 188);
-			this.dataGridView1.TabIndex = 0;
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(66, 284);
+			this.button1.Location = new System.Drawing.Point(58, 213);
+			this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(94, 29);
+			this.button1.Size = new System.Drawing.Size(82, 22);
 			this.button1.TabIndex = 1;
 			this.button1.Text = "button1";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(58, 44);
+			this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersWidth = 51;
+			this.dataGridView1.RowTemplate.Height = 29;
+			this.dataGridView1.Size = new System.Drawing.Size(262, 141);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(327, 44);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(25, 15);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "sad";
+			// 
 			// MultipleTableContainer
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.Controls.Add(this.panel1);
+			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.Name = "MultipleTableContainer";
-			this.Size = new System.Drawing.Size(653, 456);
+			this.Size = new System.Drawing.Size(571, 342);
 			this.Load += new System.EventHandler(this.MultipleTableContainer_Load);
 			this.Paint += new System.Windows.Forms.PaintEventHandler(this.MultipleTableContainer_Paint);
 			this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MultipleTableContainer_KeyPress);
 			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
 			this.ResumeLayout(false);
 
@@ -88,5 +104,6 @@ namespace Couriers_GUI.User_Interface
 		private System.Windows.Forms.Panel panel1;
 		private System.Windows.Forms.DataGridView dataGridView1;
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
 	}
 }
